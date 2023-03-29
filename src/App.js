6efa8b38ec204b9f9dc71bca35eef8b432f4f4b2
Umair Alam <101912508+alam-umair@users.noreply.github.com>
2023-03-29 10:51:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { useEffect, useContext } from "react";
+import BookCreate from "./components/BookCreate";
+import BooksContext from "./context/book";
+import BookList from "./components/BookList";
+
+function App() {
+  const { fetchBooks } = useContext(BooksContext);
+  useEffect(() => {
+    fetchBooks();
+  }, [fetchBooks]);
+
+  return (
+    <div className="app">
+      <h1>Reading List</h1>
+      <BookList />
+      <BookCreate />
+    </div>
+  );
+}
+
+export default App;
